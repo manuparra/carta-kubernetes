6805fcb7132b5661460123c84083f7d8fb5e6f42
root <root@localhost.localdomain>
2022-11-07 13:47:59 +0100

    Update version working

--- carta-docker/start.sh
+++ carta-docker/start.sh
@@ -1,11 +1,14 @@
 cd carta-controller
 docker build . --tag carta-controller
-docker run --name carta-ldap --detach osixia/openldap:1.5.0
-docker run --name carta-mongodb -d mongo
+docker run -it -d --name carta-ldap -p 389:389 -p 636:636  --detach larrycai/openldap
+docker run -it -d --name carta-mongodb -p 27017:27017  -d mongo
+sleep 5
+cd ..
 cd carta-nginx
 docker build . --tag carta-nginx
-docker run -it -p 8081:8081 --name carta-nginx carta-nginx
-docker run -it -p 8000:8000 --name carta-controller carta-controller
+docker run -it -d  -p 8000:8000 --name carta-controller carta-controller
+sleep 5
+docker run -it -d -p 8081:8081 --name carta-nginx carta-nginx
 
 
 
--- carta-docker/stop.sh
+++ carta-docker/stop.sh
@@ -3,6 +3,12 @@ docker stop carta-ldap
 docker stop carta-nginx
 docker stop carta-mongodb
 
+docker rm -f carta-controller
+docker rm -f carta-ldap
+docker rm -f carta-nginx
+docker rm -f carta-mongodb
+
+
 
 
 
